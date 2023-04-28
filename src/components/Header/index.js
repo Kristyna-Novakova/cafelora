ad82a01835a49ab1b050309ed623f0b846433f7a
Kristýna Nováková <novakovkri@gmail.com>
2023-04-28 12:38:37 +0200

    basic components

---
+++ src/components/Header/index.js
@@ -0,0 +1,31 @@
+import './style.css';
+
+export const Header = () => {
+  const element = document.createElement('header');
+  element.innerHTML = `
+      <div class="header__content container">
+      <div class="site-logo"></div>
+
+      <div class="navigation">
+        <button class="nav-btn"></button>
+        <nav class="rollout-nav nav-closed">
+          <a href="#home">domů</a>
+          <a href="#menu">menu</a>
+          <a href="#gallery">galerie</a>
+          <a href="#contact">kontakt</a>
+        </nav>
+      </div>
+
+    </div>
+  `;
+
+  // vyberte ikonku .nav-btn a připojte k ní posluchač události click
+  const navBtn = element.querySelector('.nav-btn');
+  const navRollout = element.querySelector('.rollout-nav');
+  navBtn.addEventListener('click', () => {
+    // přidej nebo odeber třídu nav-closed
+    navRollout.classList.toggle('nav-closed');
+  });
+
+  return element;
+};
