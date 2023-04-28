6ac2b5e57cf6d795a42e982feae1446af4f62e2b
Kristýna Nováková <novakovkri@gmail.com>
2023-04-28 21:05:39 +0200

    creating Layer component

--- src/components/Menu/components/Drink/index.js
+++ src/components/Menu/components/Drink/index.js
@@ -1,30 +1,24 @@
 import './style.css';
 
 export const Drink = (props) => {
-  const { name, image } = props;
+  const { name, image, layers } = props;
 
   const element = document.createElement('div');
   element.classList.add('drink');
   element.innerHTML = `
-                  <div class="drink__product">
-                  <div class="drink__cup">
-                    <img src="${cupImageSrc}">
-                  </div>
-                  <div class="drink__info">
-                    <h3>${name}</h3>
-                    <div class="layer">
-                      <div
-                        class="layer__color"
-                        style="background-color: #613916"
-                      ></div>
-                      <div class="layer__label">espresso</div>
-                    </div>
-                  </div>
-                </div>
-                <div class="drink__controls">
-                  <button class="order-btn">Objednat</button>
-                </div>
-  `;
+        <div class="drink__product">
+        <div class="drink__cup">
+          <img src="${image}">
+        </div>
+        <div class="drink__info">
+          <h3>${name}</h3>
+          ${layers.map((layer) => Layer(layer)).join('')}
+        </div>
+      </div>
+      <div class="drink__controls">
+        <button class="order-btn">Objednat</button>
+      </div>
+      `;
 
   return element;
 };
---
+++ src/components/Menu/components/Layer/index.js
@@ -0,0 +1,13 @@
+export const Layer = () => {
+  const { color, label } = props;
+
+  const element = document.createElement('div');
+  element.classList.add('layer');
+  element.innerHTML = `
+      <div class="layer__color" style="background-color: ${color}"></div>
+      <div class="layer__label">${label}</div>
+  
+  `;
+
+  return element;
+};
