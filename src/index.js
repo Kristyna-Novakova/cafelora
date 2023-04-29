8e16530962f01af63e7adb90ba11293ff419df86
Kristýna Nováková <novakovkri@gmail.com>
2023-04-29 16:26:58 +0200

    adding HomePage component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import './style.css';
-import { Header } from './components/Header/index.js';
-import { Banner } from './components/Banner/index.js';
-import { Menu } from './components/Menu/index.js';
-import { Gallery } from './components/Gallery/index.js';
-import { Contact } from './components/Contact/index.js';
-import { Footer } from './components/Footer/index.js';
+import { HomePage } from './pages/HomePage/index.js';
 
 console.log('funguju!');
 
-const pageElement = document.createElement('div');
-pageElement.setAttribute('id', 'home');
-pageElement.classList.add('page');
+// const pageElement = document.createElement('div');
+// pageElement.setAttribute('id', 'home');
+// pageElement.classList.add('page');
 
-const main = document.createElement('main');
-main.append(Banner(), Menu({ drinks: 'loading' }), Gallery(), Contact());
+// const main = document.createElement('main');
+// main.append(Banner(), Menu({ drinks: 'loading' }), Gallery(), Contact());
 
-pageElement.append(Header(), main, Footer());
+// pageElement.append(Header(), main, Footer());
 
-document.querySelector('#app').append(pageElement);
+const { pathname } = window.location;
+
+if (pathname === '/') {
+  document.querySelector('#app').append(HomePage());
+}
---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,19 @@
+import { Header } from '../../components/Header/index.js';
+import { Banner } from './components/Banner/index.js';
+import { Menu } from './components/Menu/index.js';
+import { Gallery } from './components/Gallery/index.js';
+import { Contact } from './components/Contact/index.js';
+import { Footer } from '../../components/Footer/index.js';
+
+export const HomePage = () => {
+  const pageElement = document.createElement('div');
+  pageElement.setAttribute('id', 'home');
+  pageElement.classList.add('page');
+
+  const main = document.createElement('main');
+  main.append(Banner(), Menu({ drinks: 'loading' }), Gallery(), Contact());
+
+  pageElement.append(Header(), main, Footer());
+
+  return pageElement;
+};
