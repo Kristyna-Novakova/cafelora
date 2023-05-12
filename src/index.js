72b7dbb2f4ba85aaf0bb8854018f1015629eb193
Kristýna Nováková <novakovkri@gmail.com>
2023-05-12 20:52:58 +0200

    publication to GitHUb Pages

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ console.log('funguju!');
 
 const { pathname } = window.location;
 
-if (pathname === '/') {
+if (pathname === BASE_PATH + '/') {
   document.querySelector('#app').append(HomePage());
-} else if (pathname === '/objednavka') {
+} else if (pathname === BASE_PATH + '/objednavka') {
   document.querySelector('#app').append(OrderPage());
 }
