9a53594866a51fbcda6f7d103d2786c77f2d3d1e
Kristýna Nováková <novakovkri@gmail.com>
2023-05-12 21:35:29 +0200

    changing a href

--- src/pages/HomePage/components/Menu/index.js
+++ src/pages/HomePage/components/Menu/index.js
@@ -9,17 +9,16 @@ export const Menu = (props) => {
   element.classList.add('menu');
 
   element.innerHTML = `
-  <<div class="container">
-  <h2>Naše nabídka</h2>
-  <p class="menu-intro">
-    Vyberte si z našeho interaktivního menu a nemusíte čekat na obsluhu
-  </p>
-  <div class="drinks-list"></div>
-
-  <div class="order-detail">
-    <a href="objednavka">Detail objednávky</a>
+  <div class="container">
+    <h2>Naše nabídka</h2>
+    <p class="menu-intro">
+      Vyberte si z našeho interaktivního menu a nemusíte čekat na obsluhu
+    </p>
+    <div class="drinks-list"></div>
+    <div class="order-detail">
+      <a href="objednavka">Detail objednávky</a>
+    </div>
   </div>
-</div>  
 `;
 
   const drinksList = element.querySelector('.drinks-list');
