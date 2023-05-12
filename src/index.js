28b65aa8e48bb36048aa786402ef59ab04197ba1
Kristýna Nováková <novakovkri@gmail.com>
2023-05-12 21:12:11 +0200

    final adjustments

--- src/index.js
+++ src/index.js
@@ -17,6 +17,6 @@ const { pathname } = window.location;
 
 if (pathname === BASE_PATH + '/') {
   document.querySelector('#app').append(HomePage());
-} else if (pathname === BASE_PATH + '/objednavka') {
+} else if (pathname === BASE_PATH + 'objednavka') {
   document.querySelector('#app').append(OrderPage());
 }
