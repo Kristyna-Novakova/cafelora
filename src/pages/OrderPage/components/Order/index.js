cfff64b91a061bd045b1d555837ff2ad069ea31d
Kristýna Nováková <novakovkri@gmail.com>
2023-05-12 17:17:47 +0200

    final results

--- src/pages/OrderPage/components/Order/index.js
+++ src/pages/OrderPage/components/Order/index.js
@@ -24,8 +24,12 @@ export const Order = (props) => {
     })
       .then((response) => response.json())
       .then((data) => {
-        const orderedItems = data.result.filter((item) => item.ordered);
-        if (orderedItems === true) {
+        const orderedItems = data.result.filter(
+          (item) => item.ordered === true,
+        );
+        console.log(orderedItems);
+
+        if (orderedItems.length > 0) {
           orderedItems.forEach((item) => {
             const orderComponent = OrderItem({
               name: item.name,
@@ -33,6 +37,7 @@ export const Order = (props) => {
             });
             orderItems.append(orderComponent);
           });
+
           element
             .querySelector('.empty-order')
             .classList.add('empty-order--hide');
@@ -42,6 +47,66 @@ export const Order = (props) => {
             .classList.remove('empty-order--hide');
         }
       });
+  } else {
+    items.forEach((item) => {
+      const orderComponent = OrderItem({
+        name: item.name,
+        image: item.image,
+      });
+      orderItems.append(orderComponent);
+    });
+
+    element.querySelector('.empty-order').classList.add('empty-order--hide');
   }
+
   return element;
 };
+
+// export const Order = (props) => {
+//   const { items } = props;
+
+//   const orderElm = document.createElement('div');
+//   orderElm.classList.add('order__content');
+//   orderElm.classList.add('container');
+
+//   orderElm.innerHTML = `
+//   <h1>Vaše objedávnka</h1>
+//   <p class="empty-order empty-order--hide">Zatím nemáte nic objednáno</p>
+//   <div class="order__items"></div>`;
+
+//   if (items === 'loading') {
+//     fetch('https://cafelora.kodim.app/api/me/drinks', {
+//       method: 'GET',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization: `Bearer ${localStorage.getItem('token')}`,
+//       },
+//     })
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((data) => {
+//         const orderedDrinks = data.result.filter(
+//           (drink) => drink.ordered === true,
+//         );
+//         console.log(orderedDrinks);
+//         if (orderedDrinks.length === 0) {
+//           console.log('Prázdná objednávka');
+//           orderElm
+//             .querySelector('.empty-order')
+//             .classList.remove('empty-order--hide');
+//         } else {
+//           orderElm.replaceWith(Order({ items: orderedDrinks }));
+//         }
+//       });
+//   } else {
+//     orderElm
+//       .querySelector('.order__items')
+//       .append(
+//         ...items.map((item) =>
+//           OrderItem({ name: item.name, image: item.image }),
+//         ),
+//       );
+//   }
+//   return orderElm;
+// };
--- src/pages/OrderPage/components/OrderItem/index.js
+++ src/pages/OrderPage/components/OrderItem/index.js
@@ -1,8 +1,11 @@
+import './style.css';
+
 export const OrderItem = (props) => {
   const { name, image } = props;
 
-  const element = document.querySelector('div');
+  const element = document.createElement('div');
   element.classList.add('order__item');
+
   element.innerHTML = `
   <img src="${image}" class="order-item__image">
   <div class="order-item__name">${name}</div>
